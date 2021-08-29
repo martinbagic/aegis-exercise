cedf9f37f692fb44381e1440042752c495ea31fa
Martin Bagic <martin.bagic@outlook.com>
2021-08-29 16:21:07 +0200

    Add testing example

--- src/aegis/classes/popgenstats.py
+++ src/aegis/classes/popgenstats.py
@@ -1,17 +1,21 @@
 from statistics import harmonic_mean
 
 
-class PopgenStats:
-    def __init__(self):
-        pass
+def get_Ne(pop_size_history):
+    return harmonic_mean(pop_size_history)
 
-    def analyze(self, population):
-        """Calculate and return population genetic statistics"""
 
-        def pop_size():
-            return population.pop_size_history[-1]
+# class PopgenStats:
+#     def __init__(self):
+#         pass
 
-        def pop_size_effective():
-            return harmonic_mean(population.pop_size_history)
+#     def analyze(self, population):
+#         """Calculate and return population genetic statistics"""
 
-        return (pop_size(), pop_size_effective())
+#         def pop_size():
+#             return population.pop_size_history[-1]
+
+#         def pop_size_effective():
+#             return harmonic_mean(population.pop_size_history)
+
+#         return (pop_size(), pop_size_effective())
