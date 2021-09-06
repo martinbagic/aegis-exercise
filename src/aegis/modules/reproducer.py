cb4dc038bcdcb2d5f2f8bd86f2ff3db40882ec62
Martin Bagic <martin.bagic@outlook.com>
2021-09-06 23:28:00 +0200

    Add testing for reproducer

--- src/aegis/modules/reproducer.py
+++ src/aegis/modules/reproducer.py
@@ -6,8 +6,15 @@ from aegis.panconfiguration import pan
 class Reproducer:
     def __init__(self, RECOMBINATION_RATE, MUTATION_RATIO, REPRODUCTION_MODE):
         self.RECOMBINATION_RATE = RECOMBINATION_RATE
-        self.MUTATION_RATIO = MUTATION_RATIO
         self.REPRODUCTION_MODE = REPRODUCTION_MODE
+
+        # Mutation rates
+        self.rate_0to1 = MUTATION_RATIO / (1 + MUTATION_RATIO)
+        self.rate_1to0 = 1 / (1 + MUTATION_RATIO)
+
+    # =============
+    # RECOMBINATION
+    # =============
 
     def _recombine(self, genomes):
         """Return recombined genomes"""
@@ -51,19 +58,23 @@ class Reproducer:
 
         return recombined[::2]  # Look at first comment in the function
 
-    def _assort(self, genomes):
-        """Return assorted chromatids"""
+    # ==========
+    # ASSORTMENT
+    # ==========
 
+    def _get_order(self, n_gametes=None, order=None):
         # Extract parent indices twice, and shuffle
-        order = np.repeat(np.arange(len(genomes)), 2)
-        pan.rng.shuffle(order)
+        if order is None:
+            order = np.repeat(np.arange(n_gametes), 2)
+            pan.rng.shuffle(order)
 
         # Check for selfing (selfing when pair contains equal parent indices)
         selfed = (order[::2] == order[1::2]).nonzero()[0] * 2
 
         if len(selfed) == 1:
-            # If one parent index pair is selfed, swap first selfed chromatid with the first chromatid of the previous or next pair
-            offset = -2 if selfed[0] > 0 else 2
+            # If one parent index pair is selfed,
+            #   swap first selfed chromatid with the first chromatid of the previous or next pair
+            offset = 2 if selfed[0] == 0 else -2
             order[selfed], order[selfed + offset] = (
                 order[selfed + offset],
                 order[selfed],
@@ -71,6 +82,14 @@ class Reproducer:
         elif len(selfed) > 1:
             # If multiple parent index pairs are selfed, shift first chromatid of selfed pairs
             order[selfed] = order[np.roll(selfed, 1)]
+
+        return order
+
+    def _assort(self, genomes, order=None):
+        """Return assorted chromatids"""
+
+        if order is None:
+            order = self._get_order(n_gametes=len(genomes))
 
         # Extract gametes
         gametes = genomes[order]
@@ -82,26 +101,32 @@ class Reproducer:
 
         return children, order
 
-    def _mutate(self, genomes, muta_prob):
+    # ========
+    # MUTATION
+    # ========
 
-        random_probabilities = pan.rng.random(genomes.shape)
+    def _mutate(self, genomes, muta_prob, random_probabilities=None):
+
+        if random_probabilities is None:
+            random_probabilities = pan.rng.random(genomes.shape)
 
         # Broadcast to fit [individual, chromatid, locus, bit] shape
         mutation_probabilities = muta_prob[:, None, None, None]
 
-        rate_0to1 = self.MUTATION_RATIO / (1 + self.MUTATION_RATIO)
-        rate_1to0 = 1 / (1 + self.MUTATION_RATIO)
-
         mutate_0to1 = (genomes == 0) & (
-            random_probabilities < (mutation_probabilities * rate_0to1)
+            random_probabilities < (mutation_probabilities * self.rate_0to1)
         )
         mutate_1to0 = (genomes == 1) & (
-            random_probabilities < (mutation_probabilities * rate_1to0)
+            random_probabilities < (mutation_probabilities * self.rate_1to0)
         )
 
-        mutate = mutate_0to1 + mutate_1to0
+        mutate_either = mutate_0to1 + mutate_1to0
 
-        return np.logical_xor(genomes, mutate)
+        return np.logical_xor(genomes, mutate_either)
+
+    # ================
+    # CALLING FUNCTION
+    # ================
 
     def mate(self, genomes, muta_prob):
 
