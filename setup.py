0bd5738fb894883e2b1a93c5b24a3e802783437c
Martin Bagic <martin.bagic@outlook.com>
2021-09-08 13:53:13 +0200

    Update setup.py description

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ import pathlib
 
 setuptools.setup(
     name="aegis-sim",
-    version="2.0.1",
-    description="Simulation tool for studying the evolution of life history",
+    version="2.0.0",
+    description="Simulation tool for studying the evolution of life history trait evolution",
     long_description=(pathlib.Path(__file__).parent / "README.md").read_text(),
     long_description_content_type="text/markdown",
     author="Martin Bagic, Will Bradshaw, Arian Sajina and Dario Valenzano",
